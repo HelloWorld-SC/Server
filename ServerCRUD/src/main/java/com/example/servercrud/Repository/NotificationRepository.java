771e87ce6af83804cead344bf1e1889a3e7c4258
정윤선 <144050190+Whiterangi@users.noreply.github.com>
2024-02-06 22:51:42 +0900

    Update NotificationRepository.java
    주석 변경

--- ServerCRUD/src/main/java/com/example/servercrud/Repository/NotificationRepository.java
+++ ServerCRUD/src/main/java/com/example/servercrud/Repository/NotificationRepository.java
@@ -1,7 +1,7 @@
 package com.example.servercrud.Repository;
 
 import com.example.servercrud.Entity.Notification;
-import com.example.servercrud.Entity.User; // 변경된 부분
+import com.example.servercrud.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
